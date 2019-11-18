db034cf7f5c4c0d4ed4c37a97bf8eb44cbd13b56
ascutariu <andreiscutariu92@gmail.com>
2019-11-18 23:00:11 +0200

    updates

---
+++ Week07/LinqAndLamdaExpressions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLamdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allUsers = ReadUsers("users.json");
+            var allPosts = ReadPosts("posts.json");
+        }
+
+        private static List<Post> ReadPosts(string file)
+        {
+            return ReadData.ReadFrom<Post>(file);
+        }
+
+        private static List<User> ReadUsers(string file)
+        {
+            return ReadData.ReadFrom<User>(file);
+        }
+
+    }
+}
---
+++ Week07/LinqAndLamdaExpressions/User.cs
@@ -0,0 +1,13 @@
+﻿namespace LinqAndLamdaExpressions
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
