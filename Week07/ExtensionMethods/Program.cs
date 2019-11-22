c87c55d724d08c08401a57be0ece5b3f203aaf14
Andrei Scutariu <andrei.scutariu@endava.com>
2019-11-22 18:43:57 +0200

    add extension methods

--- Week07/ExtensionMethods/Program.cs
+++ Week07/ExtensionMethods/Program.cs
@@ -1,15 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExtensionMethods
+﻿namespace ExtensionMethods
 {
-    class Program
+    using System;
+    using System.Diagnostics.Eventing.Reader;
+
+    // TODO
+    // - decimal 1299.321312
+    // - decimal 123512321.321321
+    // x.FormatWith2Dec() 1299.32, 123512321.32
+
+    public static class DecimalExtensions
     {
-        static void Main(string[] args)
+        public static string FormatWith2Dec(this decimal d)
         {
+            return d.ToString("##.##");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            decimal d = 1299.321312m;
+            string newD = d.FormatWith2Dec();
+            Console.WriteLine(newD);
+
+            //int i = 10;
+
+            ////bool result = i.IsGreaterThan(100);
+
+            //bool result = Helpers.CompareNumbers(i, 100);
+            //Console.WriteLine(result);
+
+            ////bool result2 = ExtensionMethods.IsGreaterThan(i, 100);
+            //bool result3 = i.IsGreaterThan(100);
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static bool IsGreaterThan(this int a, int b)
+        {
+            if (a > b)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class Helpers
+    {
+        public static bool CompareNumbers(int a, int b)
+        {
+            if (a > b)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
