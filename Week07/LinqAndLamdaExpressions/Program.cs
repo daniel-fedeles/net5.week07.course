24b776a6414f5adb8921116420c6016c456fd00a
Daniel Fedeles <daniel.fedeles@tss-yonder.com>
2019-11-25 22:40:15 +0200

    ......

--- Week07/LinqAndLamdaExpressions/Program.cs
+++ Week07/LinqAndLamdaExpressions/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace LinqAndLamdaExpressions
 {
+    using Models;
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using Models;
 
     internal class Program
     {
@@ -26,10 +26,10 @@
 
             var userNames2 = allUsers.Select(user => user.Name);
 
-            foreach (var value in userNames2)
-            {
-                Console.WriteLine(value);
-            }
+            //            foreach (var value in userNames2)
+            //            {
+            //                Console.WriteLine(value);
+            //            }
 
             IEnumerable<Company> allCompanies = from user in allUsers
                                                 select user.Company;
@@ -39,52 +39,155 @@
                          select user;
 
             var netUser = allUsers.First(user => user.Email.Contains(".net"));
-            Console.WriteLine(netUser.Username);
+            //            Console.WriteLine(netUser.Username);
 
             #endregion
 
             // 2 - find all posts for users having email ending with ".net".
-            IEnumerable<int> usersIdsWithDotNetMails = from user in allUsers
-                                                       where user.Email.EndsWith(".net")
-                                                       select user.Id;
+            var usersIdsWithDotNetMails = from user in allUsers
+                                          where user.Email.EndsWith(".net")
+                                          select user.Id;
 
-            IEnumerable<Post> posts = from post in allPosts
-                                      where usersIdsWithDotNetMails.Contains(post.UserId)
-                                      select post;
+            var posts = allPosts.Where(post => usersIdsWithDotNetMails.Contains(post.UserId));
 
-            foreach (var post in posts)
-            {
-                Console.WriteLine(post.Id + " " + "user: " + post.UserId);
-            }
+            //            foreach (var post in posts)
+            //            {
+            //                Console.WriteLine(post.Id + " " + "user: " + post.UserId);
+            //            }
 
             // 3 - print number of posts for each user.
 
+            var nrOfPosts = allUsers.Join(allPosts, u => u.Id, p => p.UserId, (u, p) => new { u, p })
+                        .GroupBy(@t => new { @t.u.Username, @t.p.UserId }, @t => @t.u)
+                        .Select(all => new { userName = all.Key.Username, Posts = all.Count(), });
+
+            //            foreach (var nrOfPost in nrOfPosts)
+            //            {
+            //                Console.WriteLine($"{nrOfPost.userName} {nrOfPost.Posts}");
+            //            }
 
             // 4 - find all users that have lat and long negative.
 
+            var ugn = allUsers.Where(allUser => allUser.Address.Geo.Lat < 0 && allUser.Address.Geo.Lng < 0)
+                .Select(allUser => allUser.Username);
+
+            //            foreach (var u in ugn)
+            //            {
+            //                Console.WriteLine(u);
+            //            }
 
             // 5 - find the post with longest body.
+            var postLogestBody = (allPosts.Select(p => p.Body.Length)).Max();
+            var postWithLogestBody = (allPosts.Where(p => p.Body.Length == postLogestBody));
+            //            foreach (var post in postWithLogestBody)
+            //            {
+            //                Console.WriteLine(post.Body);
+            //            }
+
 
 
             // 6 - print the name of the employee that have post with longest body.
 
+            var nameEmpWithLogBody = allUsers
+                .Join(allPosts, user => user.Id, post => post.UserId, (user, post) => new { user, post })
+                .Where(@t => @t.post.Body.Length == postLogestBody)
+                .Select(@t => @t.user.Username);
+            //            foreach (var x in nameEmpWithLogBody)
+            //            {
+            //                Console.WriteLine(x);
+            //            }
+
+
+
 
             // 7 - select all addresses in a new List<Address>. print the list.
-
+            List<Address> allAddresses = (allUsers.Select(us => us.Address)).ToList();
+            //            foreach (var allAddress in allAddresses)
+            //            {
+            //                Console.WriteLine(allAddress.Geo.Lat);
+            //                Console.WriteLine(allAddress.Geo.Lng);
+            //                Console.WriteLine(allAddress.City);
+            //                Console.WriteLine(allAddress.Street);
+            //                Console.WriteLine(allAddress.Suite);
+            //                Console.WriteLine(allAddress.Zipcode);
+            //                Console.WriteLine();
+            //        }
 
             // 8 - print the user with min lat
-
+            var minLat = (allAddresses.Select(add => add.Geo.Lat)).Min();
+            var userWithMinLat = allUsers.Where(user => user.Address.Geo.Lat.Equals(minLat));
 
             // 9 - print the user with max long
+            var maxLong = (allAddresses.Select(add => add.Geo.Lat)).Max();
+            var userWithMaxLongt = allUsers.Where(user => user.Address.Geo.Lat.Equals(maxLong));
 
 
             // 10 - create a new class: public class UserPosts { public User User {get; set}; public List<Post> Posts {get; set} }
             //    - create a new list: List<UserPosts>
             //    - insert in this list each user with his posts only
 
+            var nrOfPostsa = allUsers.Join(allPosts, u => u.Id, p => p.UserId, (u, p) => new { u, p })
+                .GroupBy(@t => new { @t.u.Username, @t.p.UserId }, @t => @t.u)
+                .Select(all => new { userName = all.Key.Username, Posts = all.Count(), });
+
+
+
+            List<UserPosts> userPostses = new List<UserPosts>();
+
+
+
+
+            var nrOfUsers = (allUsers.Select(u => u.Id)).Count();
+
+            //            Console.WriteLine(nrOfUsers);
+
+            var xxx = from post in allPosts
+                      join user in allUsers on post.UserId equals user.Id
+                      group user by new
+                      {
+                          user,
+                          post
+
+                      }
+                into allPostsForUser
+                      select new
+                      {
+                          allPostsForUser.Key.user,
+                          allPostsForUser.Key.post
+                      };
+
+
+            foreach (var nrOfPost in xxx)
+            {
+                UserPosts up = new UserPosts();
+
+                up.User = nrOfPost.user;
+                up.Posts.Add(nrOfPost.post);
+                if (!userPostses.Contains(up))
+                {
+                    userPostses.Add(up);
+                }
+
+
+
+
+            }
+
+            foreach (var up in userPostses)
+            {
+                Console.WriteLine(up.User.Username);
+                Console.WriteLine(up.Posts.Count);
+            }
+
+
+
+
             // 11 - order users by zip code
+            var usersOrderdByZip = allUsers.OrderBy(x => x.Address.Zipcode);
+
 
             // 12 - order users by number of posts
+
         }
 
         private static List<Post> ReadPosts(string file)
---
+++ Week07/LinqAndLamdaExpressions/UserPosts.cs
@@ -0,0 +1,15 @@
+﻿using LinqAndLamdaExpressions.Models;
+using System.Collections.Generic;
+
+namespace LinqAndLamdaExpressions
+{
+    public class UserPosts
+    {
+        public UserPosts()
+        {
+            Posts = new List<Post>();
+        }
+        public User User { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
